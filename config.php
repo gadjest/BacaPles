a570426f08d6577d9a6209424af0d9afb80974dc
gadjest <46781530+gadjest@users.noreply.github.com>
2019-01-18 18:58:18 +0700

    config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_2452998371396455";
+$raw_uid="3b7f12af-9f06-40a5-9c93-94cca2572de0";
+$user_name="Mas+Dayat";
+$invite_code="FBEYPXRNh9";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="538365882e4f28da";
+$device_type="Xiaomi++Redmi+Note+5A";
+
+
+?>
